e04461495206fe7bac5202f7b735d45c920ed575
ksdyb.park <ksdyb@amazon.com>
2023-07-23 10:32:05 +0900

    update

--- cdk-qa-with-rag/lib/cdk-qa-with-rag-stack.ts
+++ cdk-qa-with-rag/lib/cdk-qa-with-rag-stack.ts
@@ -1,16 +1,261 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as path from "path";
+import * as logs from "aws-cdk-lib/aws-logs"
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudFront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
+import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+
+const debug = false;
+const stage = 'dev';
+const s3_prefix = 'docs';
+const bucketName = 'bedrock-contents-storage';
+const bedrock_region = "us-west-2";
+const endpoint_url = "https://prod.us-west-2.frontend.bedrock.aws.dev";
+const model_id = "amazon.titan-tg1-large"; // amazon.titan-e1t-medium, anthropic.claude-v1
 
 export class CdkQaWithRagStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // s3 
+    const s3Bucket = new s3.Bucket(this, "chatbot-storage",{
+      bucketName: bucketName,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: false,
+      versioned: false,
+    });
+    if(debug) {
+      new cdk.CfnOutput(this, 'bucketName', {
+        value: s3Bucket.bucketName,
+        description: 'The nmae of bucket',
+      });
+      new cdk.CfnOutput(this, 's3Arn', {
+        value: s3Bucket.bucketArn,
+        description: 'The arn of s3',
+      });
+      new cdk.CfnOutput(this, 's3Path', {
+        value: 's3://'+s3Bucket.bucketName,
+        description: 'The path of s3',
+      });
+    }
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQaWithRagQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // DynamoDB for call log
+    const callLogTableName = 'db-call-log';
+    const callLogDataTable = new dynamodb.Table(this, 'dynamodb-call-log', {
+      tableName: callLogTableName,
+      partitionKey: { name: 'user-id', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'request-id', type: dynamodb.AttributeType.STRING }, 
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    const callLogIndexName = 'index-type';
+    callLogDataTable.addGlobalSecondaryIndex({ // GSI
+      indexName: callLogIndexName,
+      partitionKey: { name: 'type', type: dynamodb.AttributeType.STRING },
+    });
+
+    // DynamoDB for configuration
+    const configTableName = 'db-configuration';
+    const configDataTable = new dynamodb.Table(this, 'dynamodb-configuration', {
+      tableName: configTableName,
+      partitionKey: { name: 'user-id', type: dynamodb.AttributeType.STRING },      
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // copy web application files into s3 bucket
+    new s3Deploy.BucketDeployment(this, "upload-HTML", {
+      sources: [s3Deploy.Source.asset("../html")],
+      destinationBucket: s3Bucket,
+    });
+
+    // cloudfront
+    const distribution = new cloudFront.Distribution(this, 'cloudfront', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(s3Bucket),
+        allowedMethods: cloudFront.AllowedMethods.ALLOW_ALL,
+        cachePolicy: cloudFront.CachePolicy.CACHING_DISABLED,
+        viewerProtocolPolicy: cloudFront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      priceClass: cloudFront.PriceClass.PRICE_CLASS_200,  
+    });
+    new cdk.CfnOutput(this, 'distributionDomainName', {
+      value: distribution.domainName,
+      description: 'The domain name of the Distribution',
+    });
+
+    const roleLambda = new iam.Role(this, "role-lambda-chat", {
+      roleName: "role-lambda-for-chat",
+      assumedBy: new iam.CompositePrincipal(
+        new iam.ServicePrincipal("lambda.amazonaws.com"),
+        new iam.ServicePrincipal("bedrock.amazonaws.com"),
+      )
+    });
+    roleLambda.addManagedPolicy({
+      managedPolicyArn: 'arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole',
+    });
+    const BedrockPolicy = new iam.PolicyStatement({  // policy statement for sagemaker
+      resources: ['*'],
+      actions: ['bedrock:*'],
+    });        
+    roleLambda.attachInlinePolicy( // add bedrock policy
+      new iam.Policy(this, 'bedrock-policy-lambda-chat-bedrock', {
+        statements: [BedrockPolicy],
+      }),
+    );      
+
+    // Lambda for chat using langchain (container)
+    const lambdaChatApi = new lambda.DockerImageFunction(this, "lambda-chat", {
+      description: 'lambda for chat api',
+      functionName: 'lambda-chat-api-bedrock',
+      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../../lambda-chat')),
+      timeout: cdk.Duration.seconds(60),
+      role: roleLambda,
+      environment: {
+        bedrock_region: bedrock_region,
+        endpoint_url: endpoint_url,
+        model_id: model_id,
+        s3_bucket: s3Bucket.bucketName,
+        s3_prefix: s3_prefix,
+        callLogTableName: callLogTableName,
+        configTableName: configTableName
+      }
+    });     
+    lambdaChatApi.grantInvoke(new iam.ServicePrincipal('apigateway.amazonaws.com'));  
+    s3Bucket.grantRead(lambdaChatApi); // permission for s3
+    callLogDataTable.grantReadWriteData(lambdaChatApi); // permission for dynamo
+    configDataTable.grantReadWriteData(lambdaChatApi); // permission for dynamo
+
+    // role
+    const role = new iam.Role(this, "api-role-chatbot-bedrock", {
+      roleName: "api-role-chatbot-bedrock",
+      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com")
+    });
+    role.addToPolicy(new iam.PolicyStatement({
+      resources: ['*'],
+      actions: ['lambda:InvokeFunction']
+    }));
+    role.addManagedPolicy({
+      managedPolicyArn: 'arn:aws:iam::aws:policy/AWSLambdaExecute',
+    }); 
+
+    // API Gateway
+    const api = new apiGateway.RestApi(this, 'api-chatbot', {
+      description: 'API Gateway for chatbot',
+      endpointTypes: [apiGateway.EndpointType.REGIONAL],
+      binaryMediaTypes: ['application/pdf', 'text/plain', 'text/csv'], 
+      deployOptions: {
+        stageName: stage,
+
+        // logging for debug
+        loggingLevel: apiGateway.MethodLoggingLevel.INFO, 
+        dataTraceEnabled: true,
+      },
+    });  
+
+    // POST method
+    const chat = api.root.addResource('chat');
+    chat.addMethod('POST', new apiGateway.LambdaIntegration(lambdaChatApi, {
+      passthroughBehavior: apiGateway.PassthroughBehavior.WHEN_NO_TEMPLATES,
+      credentialsRole: role,
+      integrationResponses: [{
+        statusCode: '200',
+      }], 
+      proxy:false, 
+    }), {
+      methodResponses: [   // API Gateway sends to the client that called a method.
+        {
+          statusCode: '200',
+          responseModels: {
+            'application/json': apiGateway.Model.EMPTY_MODEL,
+          }, 
+        }
+      ]
+    }); 
+
+    if(debug) {
+      new cdk.CfnOutput(this, 'apiUrl-chat', {
+        value: api.url,
+        description: 'The url of API Gateway',
+      }); 
+      new cdk.CfnOutput(this, 'curlUrl-chat', {
+        value: "curl -X POST "+api.url+'chat -H "Content-Type: application/json" -d \'{"text":"who are u?"}\'',
+        description: 'Curl commend of API Gateway',
+      }); 
+    }
+
+    // cloudfront setting for api gateway of stable diffusion
+    distribution.addBehavior("/chat", new origins.RestApiOrigin(api), {
+      cachePolicy: cloudFront.CachePolicy.CACHING_DISABLED,
+      allowedMethods: cloudFront.AllowedMethods.ALLOW_ALL,  
+      viewerProtocolPolicy: cloudFront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    });    
+   
+    new cdk.CfnOutput(this, 'WebUrl', {
+      value: 'https://'+distribution.domainName+'/chat.html',      
+      description: 'The web url of request for chat',
+    });
+
+    new cdk.CfnOutput(this, 'UpdateCommend', {
+      value: 'aws s3 cp ../html/chat.js '+'s3://'+s3Bucket.bucketName,
+      description: 'The url of web file upload',
+    });
+
+    // Lambda - Upload
+    const lambdaUpload = new lambda.Function(this, "lambda-upload-bedrock", {
+      runtime: lambda.Runtime.NODEJS_16_X, 
+      functionName: "lambda-upload-bedrock",
+      code: lambda.Code.fromAsset("../lambda-upload"), 
+      handler: "index.handler", 
+      timeout: cdk.Duration.seconds(10),
+      logRetention: logs.RetentionDays.ONE_DAY,
+      environment: {
+        bucketName: s3Bucket.bucketName,
+        s3_prefix:  s3_prefix
+      }      
+    });
+    s3Bucket.grantReadWrite(lambdaUpload);
+    
+    // POST method - upload
+    const resourceName = "upload";
+    const upload = api.root.addResource(resourceName);
+    upload.addMethod('POST', new apiGateway.LambdaIntegration(lambdaUpload, {
+      passthroughBehavior: apiGateway.PassthroughBehavior.WHEN_NO_TEMPLATES,
+      credentialsRole: role,
+      integrationResponses: [{
+        statusCode: '200',
+      }], 
+      proxy:true, 
+    }), {
+      methodResponses: [  
+        {
+          statusCode: '200',
+          responseModels: {
+            'application/json': apiGateway.Model.EMPTY_MODEL,
+          }, 
+        }
+      ]
+    }); 
+    if(debug) {
+      new cdk.CfnOutput(this, 'ApiGatewayUrl', {
+        value: api.url+'upload',
+        description: 'The url of API Gateway',
+      }); 
+    }
+
+    // cloudfront setting for api gateway    
+    distribution.addBehavior("/upload", new origins.RestApiOrigin(api), {
+      cachePolicy: cloudFront.CachePolicy.CACHING_DISABLED,
+      allowedMethods: cloudFront.AllowedMethods.ALLOW_ALL,  
+      viewerProtocolPolicy: cloudFront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    });    
   }
 }
