ccdccae9ddae1a2dae00bdd91fe413998b137249
ksdyb.park <ksdyb@amazon.com>
2023-08-01 22:14:24 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -101,6 +101,14 @@ llm = Bedrock(model_id=modelId, client=boto3_bedrock)
 # embedding
 bedrock_embeddings = BedrockEmbeddings(client=boto3_bedrock)
 
+vectorstore = OpenSearchVectorSearch(
+    index_name = "rag-index-*",
+    is_aoss = False,
+    embedding_function = bedrock_embeddings,
+    opensearch_url=opensearch_url,
+    http_auth=(opensearch_account, opensearch_passwd),
+)
+
 enableRAG = False
 
 # load documents from s3
@@ -131,13 +139,8 @@ def load_document(file_type, s3_file_name):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100)
     texts = text_splitter.split_text(new_contents) 
     print('texts[0]: ', texts[0])
-        
-    docs = [
-        Document(
-            page_content=t
-        ) for t in texts[:3]
-    ]
-    return docs
+            
+    return texts
               
 def get_answer_using_query(query, vectorstore, rag_type):
     wrapper_store = VectorStoreIndexWrapper(vectorstore=vectorstore)
@@ -269,7 +272,21 @@ def lambda_handler(event, context):
             print('file_type: ', file_type)
             
             # load documents where text, pdf, csv are supported
-            docs = load_document(file_type, object)
+            texts = load_document(file_type, object)
+
+            docs = []
+            for i in range(len(texts)):
+                docs.append(
+                    Document(
+                        page_content=texts[i],
+                        metadata={
+                            'name': object,
+                            'page':i+1
+                        }
+                    )
+                )        
+            print('docs[0]: ', docs[0])    
+            print('docs size: ', len(docs))
                         
             if rag_type == 'faiss':
                 if enableRAG == False:                    
@@ -286,13 +303,22 @@ def lambda_handler(event, context):
                     vectorstore.merge_from(vectorstore_new) # merge 
                     print('vector store size: ', len(vectorstore.docstore._dict))
 
-            elif rag_type == 'opensearch':         
-                vectorstore = OpenSearchVectorSearch.from_documents(
-                    docs, 
-                    bedrock_embeddings, 
-                    opensearch_url=opensearch_url,
-                    http_auth=(opensearch_account, opensearch_passwd),
+            elif rag_type == 'opensearch':    
+                new_vectorstore = OpenSearchVectorSearch(
+                    index_name="rag-index-"+userId,
+                    is_aoss = False,
+                    embedding_function = bedrock_embeddings,
+                    opensearch_url = opensearch_url,
+                    http_auth = ("admin", "Wifi1234!"),
                 )
+                new_vectorstore.add_documents(docs)     
+
+                #vectorstore = OpenSearchVectorSearch.from_documents(
+                #    docs, 
+                #    bedrock_embeddings, 
+                #    opensearch_url=opensearch_url,
+                #    http_auth=(opensearch_account, opensearch_passwd),
+                #)
                 if enableRAG==False: 
                     enableRAG = True
                     
