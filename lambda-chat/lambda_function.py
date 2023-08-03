07494b574adef65ed4fea669fdb5de8010b8ce82
ksdyb.park <ksdyb@amazon.com>
2023-08-03 19:07:40 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -204,7 +204,14 @@ boto3_bedrock = bedrock.get_bedrock_client(
 modelInfo = boto3_bedrock.list_foundation_models()    
 print('models: ', modelInfo)
 
-llm = Bedrock(model_id=modelId, client=boto3_bedrock)
+parameters = {
+    "maxTokenCount":512,
+    "stopSequences":[],
+    "temperature":0,
+    "topP":0.9
+}
+
+llm = Bedrock(model_id=modelId, client=boto3_bedrock, model_kwargs=parameters)
 
 # embedding
 bedrock_embeddings = BedrockEmbeddings(client=boto3_bedrock)
