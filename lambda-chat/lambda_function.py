2e0fccfe390c7c25b7399b3e2898161602108bd9
ksdyb.park <ksdyb@amazon.com>
2023-09-09 21:33:28 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -208,11 +208,12 @@ def get_answer_using_template(query, vectorstore, rag_type):
     
     print('length of relevant_documents: ', len(relevant_documents))
     
-    prompt_template = """Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    prompt_template = """\n\nHuman: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
     {context}
 
     Question: {question}
+    
     Assistant:"""
     PROMPT = PromptTemplate(
         template=prompt_template, input_variables=["context", "question"]
@@ -285,6 +286,8 @@ def get_parameter(modelId):
             "max_tokens_to_sample":1024,
         }
 parameters = get_parameter(modelId)
+HUMAN_PROMPT = "\n\nHuman:"
+AI_PROMPT = "\n\nAssistant:"
 
 llm = Bedrock(model_id=modelId, client=boto3_bedrock, model_kwargs=parameters)
 
@@ -374,7 +377,7 @@ def lambda_handler(event, context):
                         else:
                             msg = get_answer_using_template(text, vectorstore, rag_type)                            
                     else:
-                        msg = llm(text)
+                        msg = llm(HUMAN_PROMPT+text+AI_PROMPT)
                 #print('msg: ', msg)
                 chat_memory.save_context({"input": text}, {"output": msg})
             
