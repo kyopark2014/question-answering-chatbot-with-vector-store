152e99f536f920ac05f170a09bb0a561b7010f28
ksdyb.park <ksdyb@amazon.com>
2023-09-10 16:42:39 +0900

    update

--- cdk-qa-with-rag/lib/cdk-qa-with-rag-stack.ts
+++ cdk-qa-with-rag/lib/cdk-qa-with-rag-stack.ts
@@ -27,7 +27,7 @@ const opensearch_account = "admin";
 const opensearch_passwd = "Wifi1234!";
 const accessType = "preview"; // aws or preview
 const bedrock_region = "us-east-1";  // "us-east-1" "us-west-2" 
-const enableConversationMode = 'true'; 
+const enableConversationMode = 'false'; 
 const enableReference = 'false';
 const enableRAG = 'true';
 
