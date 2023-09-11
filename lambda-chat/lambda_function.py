a70a79e2d2b5c6d5be7b090fb368c640f1dcb3df
ksdyb.park <ksdyb@amazon.com>
2023-09-11 10:52:26 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -493,7 +493,10 @@ def lambda_handler(event, context):
                 )
                 
                 for i in range(len(texts)):
-                    doc = Document(
+                    print('texts: ', texts[i])
+
+                    doc = []
+                    doc.append = Document(
                                 page_content=texts[i],
                                 metadata={
                                     'name': object,
