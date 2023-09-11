9462e46a1252989522b96dd32fdcd492a4b9e00e
ksdyb.park <ksdyb@amazon.com>
2023-09-12 08:09:23 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -84,8 +84,8 @@ def load_document(file_type, s3_file_name):
     new_contents = str(contents).replace("\n"," ") 
     print('length: ', len(new_contents))
 
-    #text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100) # English
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=250,chunk_overlap=20) # Korean
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100) # English
+    #text_splitter = RecursiveCharacterTextSplitter(chunk_size=250,chunk_overlap=20) # Korean
     texts = text_splitter.split_text(new_contents) 
     #print('texts[0]: ', texts[0])
             
