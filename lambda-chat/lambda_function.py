a80546880a6e923b2433685b21fb12bbe71a2679
ksdyb.park <ksdyb@amazon.com>
2023-09-16 12:38:57 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -366,7 +366,7 @@ def create_ConversationalRetrievalChain(vectorstore):
             search_type="similarity", search_kwargs={"k": 3}
         ),         
         condense_question_prompt=CONDENSE_QUESTION_PROMPT, # chat history and new question
-        combine_docs_chain_kwargs={'prompt': qa_prompt_template},  
+        #combine_docs_chain_kwargs={'prompt': qa_prompt_template},  
 
         memory=memory_chain,
         get_chat_history=_get_chat_history,
@@ -378,7 +378,7 @@ def create_ConversationalRetrievalChain(vectorstore):
         #return_source_documents=True, # retrieved source
         #return_generated_question=False, # generated question
     )
-    #qa.combine_docs_chain.llm_chain.prompt = PromptTemplate.from_template(qa_prompt_template) 
+    qa.combine_docs_chain.llm_chain.prompt = PromptTemplate.from_template(qa_prompt_template) 
     
     return qa
 
