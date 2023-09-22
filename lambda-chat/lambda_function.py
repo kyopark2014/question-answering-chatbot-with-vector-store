af2b94ade4ac2b122d9ff6a3ad961a38c0f5b4ee
ksdyb.park <ksdyb@amazon.com>
2023-09-23 00:42:05 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -518,13 +518,27 @@ def get_answer_using_template(query, vectorstore, rag_type):
     print('length of relevant_documents: ', len(relevant_documents))
 
     # check korean
-    prompt_template = """\n\nHuman: Using the following conversation, answer friendly for the newest question. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    pattern_hangul = re.compile('[\u3131-\u3163\uac00-\ud7a3]+') 
+    word_kor = pattern_hangul.search(str(query))
+    print('word_kor: ', word_kor)
+    
+    if word_kor:
+        #prompt_template = """\n\nHuman: 아래 문맥(context)을 참조했음에도 답을 알 수 없다면, 솔직히 모른다고 말합니다.
+        prompt_template = """\n\nHuman: 아래 문맥(context)을 참조했음에도 답을 알 수 없다면, 솔직히 모른다고 말합니다. Assistant은 상황에 맞는 구체적인 정보를 친절하게 답변합니다.
         
-    {context}
+        {context}
+        
+        Question: {question}
 
-    Question: {question}
+        Assistant:"""
+    else:
+        prompt_template = """\n\nHuman: Using the following conversation, answer friendly for the newest question. If you don't know the answer, just say that you don't know, don't try to make up an answer. You will be acting as a thoughtful advisor.
+        
+        {context}
 
-    Assistant:"""
+        Question: {question}
+
+        Assistant:"""
     
     PROMPT = PromptTemplate(
         template=prompt_template, input_variables=["context", "question"]
