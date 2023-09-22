568e54e3df31af3df3beaabb000476d18b187cb2
ksdyb.park <ksdyb@amazon.com>
2023-09-22 20:55:25 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -434,7 +434,7 @@ def get_answer_using_template(query, vectorstore, rag_type):
     print('word_kor: ', word_kor)
     
     if word_kor:
-        prompt_template = """\n\nHuman: 다음은 Human과 Assistant의 친근한 대화입니다. Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다. Assistant는 모르는 질문을 받으면 솔직히 모른다고 말합니다.
+        prompt_template = """\n\nHuman: 다음은 Human과 Assistant의 친근한 대화입니다. Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다. Assistant는 모르는 질문을 받으면, "주어진 내용에서 관련 답변을 찾을 수 없습니다."라고 말합니다.
     
         <context>
         {context}
@@ -444,7 +444,7 @@ def get_answer_using_template(query, vectorstore, rag_type):
 
         Assistant:"""
     else:
-        prompt_template = """\n\nHuman: Using the following conversation, answer friendly for the newest question. If you don't know the answer, just say that you don't know, don't try to make up an answer. You will be acting as a thoughtful advisor.
+        prompt_template = """\n\nHuman: Using the following conversation, answer friendly for the newest question. You will be acting as a thoughtful advisor. If the answer is not in the context, say "주어진 내용에서 관련 답변을 찾을 수 없습니다."
         
         <context>
         {context}
