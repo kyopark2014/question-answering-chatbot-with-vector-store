97162ba9be089f1cf5284c6aa8e3b2b7600bd473
ksdyb.park <ksdyb@amazon.com>
2023-09-23 09:23:46 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -94,7 +94,11 @@ def get_parameter(modelId):
         }
     elif modelId == 'anthropic.claude-v1' or modelId == 'anthropic.claude-v2':
         return {
+            "stop_sequences": [HUMAN_PROMPT],
             "max_tokens_to_sample":1024,
+            "temperature":0.1,
+            "top_k":3,
+            "top_p": 0.1
         }
 parameters = get_parameter(modelId)
 HUMAN_PROMPT = "\n\nHuman:"
