274ad59146793ba0768013be6c38395f326e01b4
ksdyb.park <ksdyb@amazon.com>
2023-09-24 19:28:20 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -299,7 +299,7 @@ def get_answer_using_template_with_history(query, vectorstore, rag_type, chat_me
         relevant_documents = vectorstore.similarity_search_by_vector(query_embedding)
     elif rag_type == 'opensearch':
         kwargs = {"score_threshold":0.8}
-        relevant_documents = vectorstore.similarity_search(query=query, k=3, **kwargs)
+        relevant_documents = vectorstore.similarity_search_with_relevance_scores(query=query, k=3, **kwargs)
     print('relevant_documents: ', relevant_documents)
 
     print(f'{len(relevant_documents)} documents are fetched which are relevant to the query.')
