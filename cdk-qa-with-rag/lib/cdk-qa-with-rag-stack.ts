f8a6abfeb0bde7f93facb6dff7f02f792595a996
Kyoungsu Park <52392004+kyopark2014@users.noreply.github.com>
2023-10-16 11:29:51 +0900

    Update cdk-qa-with-rag-stack.ts

--- cdk-qa-with-rag/lib/cdk-qa-with-rag-stack.ts
+++ cdk-qa-with-rag/lib/cdk-qa-with-rag-stack.ts
@@ -36,7 +36,7 @@ export class CdkQaWithRagStack extends cdk.Stack {
 
     // s3 
     const s3Bucket = new s3.Bucket(this, `storage-${projectName}-${region}`,{
-      bucketName: bucketName,
+      // bucketName: bucketName,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
